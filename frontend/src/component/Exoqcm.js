f32c6bade3903337807337bd84edeb5d5a78a418
Riadh <riadhsoumari2016@gmail.com>
2023-07-03 15:26:50 +0200

    maj qcm

--- frontend/src/component/Exoqcm.js
+++ frontend/src/component/Exoqcm.js
@@ -7,12 +7,26 @@ const Exoqcm = () => {
   return (
     <div>
     <div class="bga">
-      <Navigation></Navigation>
+      <div class="bandeau">
+        <div class="chrono">
+          10:00
+          </div>
       <div class="qcmtheme">
       <h1 class="htext">Question à choix multiples n°X</h1>
+      </div>
+      </div>
+      </div>
+      <div class="bandeau">
       <p class= "stext">Consigne : Questionnaire à choix multiple. Choisissez une et une seule proposition de réponse.</p>
+      <div class="upright">
+          <button class="button2">
+          Exercice 5/15
+          </button>
+          <button class="button2" style={{backgroundColor:"#FBE62F"}}>Me rappeler la règle</button>
+
+          </div>
       </div>
-      </div>
+      
       <div class="qcm-container">
       <Question></Question>
       <Question></Question>
